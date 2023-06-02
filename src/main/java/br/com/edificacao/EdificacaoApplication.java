0012ece4ef626f15ea6b8d92fef51c435684e2c9
Alex Camaroti <camarottii@hotmail.com>
2023-06-02 14:35:32 -0300

    Incluido projeto base com config do banco postgres

---
+++ src/main/java/br/com/edificacao/EdificacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.edificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/edificacao/EdificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
